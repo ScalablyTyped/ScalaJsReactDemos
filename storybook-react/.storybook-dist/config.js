442865cf3989e2d1c5a34a4faf6d728466b0b9f2
Øyvind Raddum Berg <elacin@gmail.com>
2021-07-10 07:03:58 +0200

    dist

--- storybook-react/.storybook-dist/config.js
+++ storybook-react/.storybook-dist/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-    require('../target/scala-2.13/storybook-react-opt.js');
+    require('../target/scala-3.0.1/storybook-react-opt.js');
 }
 
 configure(loadStories, module);
--- storybook-react/.storybook/config.js
+++ storybook-react/.storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-    require('../target/scala-3.0.0/storybook-react-fastopt.js');
+    require('../target/scala-3.0.1/storybook-react-fastopt.js');
 }
 
 configure(loadStories, module);
